899c530e9a0260a630bbf2a0c969644a44dde4aa
Jacob Wegner <jacobwegner@gmail.com>
2014-07-07 08:15:18 -0500

    Commit

--- pinax_theme_bootstrap_account/context_processors.py
+++ pinax_theme_bootstrap_account/context_processors.py
@@ -2,6 +2,9 @@ from pinax_theme_bootstrap_account.conf import settings
 
 
 def theme(request):
+    """
+    Now adding some other code in the master branch.
+    """
     ctx = {
         "THEME_ACCOUNT_ADMIN_URL": settings.THEME_ACCOUNT_ADMIN_URL,
         "THEME_ACCOUNT_CONTACT_EMAIL": settings.THEME_ACCOUNT_CONTACT_EMAIL,
