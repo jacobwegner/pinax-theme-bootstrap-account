eaaf55815e89612b531130048fc00a45f293e4cd
Jacob Wegner <jacobwegner@gmail.com>
2014-07-07 08:22:33 -0500

    Push master ahead of branch

--- pinax_theme_bootstrap_account/context_processors.py
+++ pinax_theme_bootstrap_account/context_processors.py
@@ -6,6 +6,8 @@ def theme(request):
     Now adding some other code in the master branch.
 
     More changes here.
+
+    And now the master is ahead of the branch.
     """
     ctx = {
         "THEME_ACCOUNT_ADMIN_URL": settings.THEME_ACCOUNT_ADMIN_URL,
